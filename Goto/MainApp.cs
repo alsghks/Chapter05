80cd84ddc985660c64b97a062fef6a5e09fe5c3f
민환 이 <dbjenjoy@gmail.com>
2023-03-17 15:44:08 +0900

    230317-수업

---
+++ Goto/MainApp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Goto
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("종료 조건(숫자)을 입력하세요 :");
+
+            String input = Console.ReadLine();
+
+            int input_number = Convert.ToInt32(input);
+            int exit_number = 0;
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int k = 0; k < 3; k++)
+                {
+                    if (exit_number++ == input_number)
+                    {
+                        goto EXIT_FOR;
+                    }                        
+
+                    Console.WriteLine(exit_number);
+                }
+            }
+            goto Exit_Program;
+
+        EXIT_FOR:
+            Console.WriteLine("\nExit nested for...");
+
+        Exit_Program:
+            Console.WriteLine("Exit_Program...");
+        }
+        
+
+
+      
+      
+
+    }
+}
